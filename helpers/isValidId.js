5c5ce960c487026b724af8fb8dee6368b4108156
Anna <anjanadvorna@gmail.com>
2024-02-01 17:48:18 +0200

    Update isValidId.js

--- helpers/isValidId.js
+++ helpers/isValidId.js
@@ -5,7 +5,7 @@ const isValidId = (req, res, next) => {
     const { id } = req.params;
     if (!isValidObjectId(id)) {
         console.log(false)
-        next(HttpError(404, `${id} is not valid id`) );
+        next(HttpError(400, `${id} is not valid id`) );
     }
     next();
 };
