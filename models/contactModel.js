aaee2b3a31cddc2eb3c748df975b18609688a3c5
Anna <anjanadvorna@gmail.com>
2024-02-04 14:34:47 +0200

    update

--- models/contactModel.js
+++ models/contactModel.js
@@ -22,18 +22,18 @@ const contactSchema = new Schema({
 
 contactSchema.post("save", handleMongooseError);
 
-const Contact = mongoose.model("contact", contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 const createContactSchema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     phone: Joi.string().required(),
     favorite: Joi.boolean(),
 });
 
 const updateContactSchema = Joi.object({
     name: Joi.string(),
-    email: Joi.string(),
+    email: Joi.string().email(),
     phone: Joi.string(),
     favorite: Joi.boolean(),
 }).min(1).message("Body must have at least one field");
