b8e3933784f9975dc204176c6941616a478e36a0
Anna <anjanadvorna@gmail.com>
2024-02-19 20:50:53 +0200

    add sendEmail

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const controllerWrapper = require("./controllerWrapper");
 const handleMongooseError = require("./hendleMongoseError");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
     HttpError,
     controllerWrapper,
     handleMongooseError,
+    sendEmail,
 }
